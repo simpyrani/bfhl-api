f9cd8d60556c91e7e426e58d29e0f0caa965520d
Simpy Rani <simpy.rani2022@vitstudent.ac.in>
2025-08-29 11:40:07 +0530

    Initial BFHL API

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // index.js
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const FULL_NAME_LOWER = process.env.FULL_NAME_LOWER;
+
 
 const app = express();
 app.use(cors());
